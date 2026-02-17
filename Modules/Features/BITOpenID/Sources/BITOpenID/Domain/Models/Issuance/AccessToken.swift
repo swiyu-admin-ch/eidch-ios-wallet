dae908ec1e300de0c60030b263073b370de263ea
b123400 <i@b123400.net>
2026-02-17 18:01:36 +0900

    Take nonce from access token
    According to the doc, a nonce is returned along with the access token
    https://github.com/swiyu-admin-ch/swiyu-issuer/blob/64e641571e764916aaf6e0aac04f9b4e27e4c12f/issuance.md#L289

--- Modules/Features/BITOpenID/Sources/BITOpenID/Domain/Models/Issuance/AccessToken.swift
+++ Modules/Features/BITOpenID/Sources/BITOpenID/Domain/Models/Issuance/AccessToken.swift
@@ -20,6 +20,7 @@ public struct AccessToken: Codable, Equatable {
     accessToken = try container.decode(String.self, forKey: .accessToken)
     expiresIn = try container.decodeIfPresent(Int.self, forKey: .expiresIn)
     refreshToken = try container.decodeIfPresent(String.self, forKey: .refreshToken)
+    nonce = try container.decodeIfPresent(String.self, forKey: .nonce)
 
     let rawType = try container.decode(String.self, forKey: .tokenType)
     guard let type = TokenType(rawValue: rawType.lowercased()) else {
@@ -41,11 +42,13 @@ public struct AccessToken: Codable, Equatable {
     case tokenType = "token_type"
     case expiresIn = "expires_in"
     case refreshToken = "refresh_token"
+    case nonce = "c_nonce"
   }
 
   let accessToken: String
   let tokenType: TokenType
   let expiresIn: Int?
   let refreshToken: String?
+  let nonce: String?
 
 }
